50394a2045ca53e59909d6d6eb27f231e4a09506
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-01-02 00:02:28 +0530

    update

--- portfolio/src/components/HomePage.jsx
+++ portfolio/src/components/HomePage.jsx
@@ -1,27 +1,50 @@
+import './HomePage.css'
+
 const HomePage = () => {
 
     return (
-        <div className='screen border-black border-2 bg-blue-600  w-full '>
+        <div id="full-body">
+            <div id="Screen">
 
-            <div className="navbar border-black border-4 h-16 bg-violet-950 w-full ">
-                <div className="navbar2 border-red-700 border-4 flex h-16 justify-between text-white w-11/12  mx-auto">
-                    {/* <div className="navLeft border-black border tracking-wide">
-                        <h1 className="align-middle font-bold text-3xl mt-2  ">portfolio</h1>
-                    </div>
-                    <div className="navRight border-white border w-2/5  flex justify-between pt-3 text-xl">
-                        <a>Home</a>
-                        <a>About</a>
-                        <a>Services</a>
-                        <a>Skills</a>
-                        <a>Projects</a>
-                        <a>Contact</a>
+                <div id="navbar">
+                    <input type='checkbox' id='check'/>
+                    <label for="check" className='checkbtn'>
+                    <i class="fa-solid fa-bars fa-xl"></i>
+                    </label>
+
+                    <div id="left-navi">
+                        <h1>PORTFOLIO.</h1>
+                    </div>  
+                    
+                    <ul id="right-navi">
+                        <li><a>HOME</a></li>
+                        <li><a>HOME</a></li>
+                        <li><a>SKILLS</a></li>
+                        <li><a>PROJECTS</a></li>
+                        <li><a>SERVICES</a></li>
+                        <li><a>CONTACT</a></li>
+                    </ul>
+                    {/* <div className='navlinks'>
+                        <button className='open' name='openlist'><i class="fa-solid fa-bars fa-xl"></i></button>
+                        <button className='close' name='closelist'><i class="fa-solid fa-xmark fa-xl"></i></button>
                     </div> */}
                 </div>
+
+                <div id='Home'>
+                    <h2 className='h2'>Hello, It's Me</h2>
+                    <h1 className='h1'>Amaan chaudhary</h1>
+                    <h2 className='h2'>I am a Full Stack Web Developer</h2>
+                    <p className='p'>
+                        As a web developer, I possess a diverse skill set
+                        covering both front-end and back-end technologies.
+                        Proficient in languages such as HTML, CSS, JavaScript , React.js and Node.js.
+                    </p>
+                </div>
+
             </div>
 
-            <div>
 
-            </div>
+
         </div>
     )
 }
